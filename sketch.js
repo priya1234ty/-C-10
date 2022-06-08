1f5595af555326996633d9d2d55d91ff9b623745
priya1234ty <107059888+priya1234ty@users.noreply.github.com>
2022-06-08 17:20:44 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,17 @@
+
+var weight = [ 45,35,65]
+
+sum = weight[0]+weight[1]+weight[2]
+average= sum/weight.length
+console.log(average)
+function setup() 
+{
+  createCanvas(400,400);
+}
+
+function draw() 
+{
+background(51);
+
+}
+
